2ee6ecd8ce0855e07d3a600204c1337097f74cce
Hema1394 <hemagiri1394.in@gmail.com>
2024-05-22 18:53:12 +0530

    Create index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+// src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
