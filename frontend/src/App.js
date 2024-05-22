61d5242bae88e51f8e74658c0db4de3ce55512be
Hema1394 <hemagiri1394.in@gmail.com>
2024-05-22 18:52:03 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+// src/App.js
+
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
