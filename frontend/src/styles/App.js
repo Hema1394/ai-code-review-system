87e2dc50390aef4896b0334b0aead37ff8a87102
Hema1394 <hemagiri1394.in@gmail.com>
2024-05-22 16:37:58 +0530

    Create App.js

---
+++ frontend/src/styles/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the AI Code Review System</h1>
+        <p>Developed by Hemagiri with ❤ of Department of AIDS</p>
+        <p>Full copyright © 2024 reserved</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
